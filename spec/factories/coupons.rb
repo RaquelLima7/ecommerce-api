26c8f94b3e1d639a828f44a340aba93d7a65b12b
RaquelLima7 <raquellimadev@gmail.com>
2022-03-05 16:08:20 -0300

    FactoryBot coupon and user

--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :coupon do
     sequence(:name) { |n| "My Coupon #{n}" }
     code { Faker::Commerce.unique.promotion_code(digits: 4) }
-    status { :active }
-    discount_value { 25 }
+    status { %i(active inactive).sample }
+    discount_value { rand(1..99) }
     max_use { 1 }
     due_date { 3.days.from_now }
   end
