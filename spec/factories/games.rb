226cd88ac57285c9eb3389ae2a02d0b602ac89ae
RaquelLima7 <raquellimadev@gmail.com>
2022-03-05 15:33:53 -0300

    model and rspec game

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :game do
-    mode { 1 }
-    release_date { "2022-03-05 15:13:46" }
-    developer { "MyString" }
-    system_requirement { nil }
+    mode { %i(pvp pve both).sample }
+    release_date { '2020-06-01' }
+    developer { Faker::Company.name }
+    system_requirement
   end
 end
