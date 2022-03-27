fcc6afa7c9c3b7c2800c38f79482ad205fc7d7f7
RaquelLima7 <raquellimadev@gmail.com>
2022-03-27 13:23:51 -0300

    added System Requirements Controller

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -1,6 +1,6 @@
 module Admin::V1
   class CategoriesController < ApiController
-    before_action :load_category, only: [:update, :destroy]
+    before_action :load_category, only: %i[update destroy]
 
     def index
       @categories = Category.all
---
+++ spec/requests/admin/v1/system_requirements/admin_request_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe "Admin V1 System Requirements as :admin", type: :request do
+
+
+end
