c9ea037c2ef4119ce0646e7273e1ced73dfcba3c
RaquelLima7 <raquellimadev@gmail.com>
2023-02-20 13:42:25 -0300

    Added commentar inauthenticatable.rb

--- app/controllers/concerns/authenticatable.rb
+++ app/controllers/concerns/authenticatable.rb
@@ -6,3 +6,10 @@ module Authenticatable
     before_action :authenticate_user!
   end
 end
+
+
+# before_action = sempre que alguém chamar o controller, vai rolar a ação antes
+# que é o authenticate_user, que vai forçar a autenticação do usuário. Todo lugar que
+# o Concern tiver incluído ele automaticamente para incluir o Devise e vai executar o
+# authenticate_user.
+# Para usar, só colocar o include Authenticatable no controller.
